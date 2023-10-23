e29b3b267cb087c158a7cf2cbb9e0738aaed1e9f
Shyam Sundar Tiwari <shyampersonal2000@gmail.com>
2023-10-23 13:18:51 +0530

    updated app.js with component names

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
 	return (
 		<div className='App'>
-			<header>This is header section</header>
-			<div className='mid-body'>This is mid-body</div>
-			<footer>This is footer.</footer>
+			<HeaderComp />
+			<MainContainer />
+			<FooterComp />
 		</div>
 	);
 }
