e76bd87533a72b7aa9776840e89f252f32e140d8
Ritik Wandale <wandaleritik101@gmail.com>
2023-10-23 13:51:27 +0530

    Folder structure Modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 // import logo from './logo.svg';
 import HeaderComp from './components/HeaderComp';
 import FooterComp from './components/FooterComp';
-import './App.css';
+import './styles/App.css';
 import MainContainer from './components/MainContainer';
-
 function App() {
 	return (
 		<div className='App'>
