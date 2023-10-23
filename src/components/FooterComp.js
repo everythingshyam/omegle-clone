2cd74a02556b06df694b71df0409fd61c503f70d
Shyam Sundar Tiwari <shyampersonal2000@gmail.com>
2023-10-23 18:43:31 +0530

    header & footer basic layout

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -1,5 +1,22 @@
 import React from 'react';
-
+import '../styles/FooterComp.css';
 export default function FooterComp() {
-	return <div>FooterComp</div>;
+	return (
+		<>
+			<ul className='footer-sub'>
+				<li>
+					<div className='footer-button'>About Omegle</div>
+				</li>
+				<li>
+					<div className='footer-button'>Support Us</div>
+				</li>
+				<li>
+					<div className='footer-button'>Developer Section</div>
+				</li>
+				<li>
+					<div className='footer-button'>Praise & Critics</div>
+				</li>
+			</ul>
+		</>
+	);
 }
--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -1,5 +1,34 @@
 import React from 'react';
+import '../styles/HeaderComp.css';
+import icon1 from '../res/icon1.png';
+import { RxHamburgerMenu } from 'react-icons/rx';
+// import { FiSettings } from 'react-icons/fi';
 
 export default function HeaderComp() {
-	return <div>HeaderComp</div>;
+	return (
+		<>
+			<header id='header'>
+				<div className='brand-container'>
+					<div className='brand-logo'>
+						<img
+							src={icon1}
+							alt='Omegle Icon'
+						/>
+					</div>
+					<div className='brand-name'>Omegle</div>
+				</div>
+				<nav className='nav-container'>
+					{/* <div>
+						<FiSettings className='setting-icon' />
+					</div> */}
+					<div>
+						<RxHamburgerMenu
+							className='nav-icon'
+							style={{ fontSize: '2rem' }}
+						/>
+					</div>
+				</nav>
+			</header>
+		</>
+	);
 }
